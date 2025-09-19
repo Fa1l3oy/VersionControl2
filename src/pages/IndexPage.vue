da572863b85c86d4ae5ea0a944260f0d8a5b69c9
sunshine <monthanomrat213@gmail.com>
2025-09-20 00:58:51 +0700

    secrets.GITHUB_TOKEN

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="q-pa-md" style="max-width: 400px">
-
     <q-form
       @submit="onSubmit"
       @reset="onReset"
@@ -9,17 +8,17 @@
       <q-input
         filled
         v-model="name"
-        label="Your name *"
+        label="Your name"
         hint="Name and surname"
         lazy-rules
-        :rules="[ val => val && val.length > 0 || 'Please type something']"
+        :rules="[val => val && val.length > 0 || 'Please type something']"
       />
 
       <q-input
         filled
         type="number"
         v-model="age"
-        label="Your age *"
+        label="Your age"
         lazy-rules
         :rules="[
           val => val !== null && val !== '' || 'Please type your age',
@@ -27,14 +26,16 @@
         ]"
       />
 
-      <q-toggle v-model="accept" label="I accept the license and terms" />
+      <q-toggle
+        v-model="accept"
+        label="I accept the license and terms"
+      />
 
       <div>
-        <q-btn label="Submit" type="submit" color="primary"/>
+        <q-btn label="Submit" type="submit" color="primary" />
         <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
       </div>
     </q-form>
-
   </div>
 </template>
 
@@ -50,35 +51,36 @@ export default {
     const age = ref(null)
     const accept = ref(false)
 
+    const onSubmit = () => {
+      if (accept.value !== true) {
+        $q.notify({
+          color: 'red-5',
+          textColor: 'white',
+          icon: 'warning',
+          message: 'You need to accept the license and terms first'
+        })
+      } else {
+        $q.notify({
+          color: 'green-4',
+          textColor: 'white',
+          icon: 'cloud_done',
+          message: 'Submitted'
+        })
+      }
+    }
+
+    const onReset = () => {
+      name.value = null
+      age.value = null
+      accept.value = false
+    }
+
     return {
       name,
       age,
       accept,
-
-      onSubmit () {
-        if (accept.value !== true) {
-          $q.notify({
-            color: 'red-5',
-            textColor: 'white',
-            icon: 'warning',
-            message: 'You need to accept the license and terms first'
-          })
-        }
-        else {
-          $q.notify({
-            color: 'green-4',
-            textColor: 'white',
-            icon: 'cloud_done',
-            message: 'Submitted'
-          })
-        }
-      },
-
-      onReset () {
-        name.value = null
-        age.value = null
-        accept.value = false
-      }
+      onSubmit,
+      onReset
     }
   }
 }
